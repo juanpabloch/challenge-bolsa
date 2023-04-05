cee1c42c1d7fb4b8f53d7c9c162028515bd2efab
JuanPablo <jchoternasty@the-8agency.com>
2023-04-05 13:33:07 -0300

    estilos

--- api/api/services/jobs.py
+++ api/api/services/jobs.py
@@ -3,8 +3,8 @@ from django.db.models import F, DecimalField, ExpressionWrapper
 
 def update_products():
     products = Products.objects.all()
-    products = products.annotate(new_buy=ExpressionWrapper(F('buy')*1.05, output_field=DecimalField()))
-    products = products.annotate(new_sell=ExpressionWrapper(F('sell')*1.05, output_field=DecimalField()))
+    products = products.annotate(new_buy=ExpressionWrapper(F('buy')*1.02, output_field=DecimalField()))
+    products = products.annotate(new_sell=ExpressionWrapper(F('sell')*1.02, output_field=DecimalField()))
     
     for obj in products:
         obj.sell = obj.new_sell
--- api/api/services/updater.py
+++ api/api/services/updater.py
@@ -3,6 +3,6 @@ from api.services.jobs import update_products
 
 def start():
     schedule = BackgroundScheduler()
-    schedule.add_job(update_products, 'interval', seconds=10)
+    schedule.add_job(update_products, 'interval', seconds=60)
     schedule.start()
     
